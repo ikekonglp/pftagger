dadbddaa960d0e069e2696010505e8ad485f24be
ikekonglp <ikekonglp@gmail.com>
2014-08-19 13:49:03 -0400

    make bash work

--- BuildCFMatrix.py
+++ BuildCFMatrix.py
@@ -9,8 +9,11 @@ import copy
 
 parser = argparse.ArgumentParser(description='')
 parser.add_argument('inputf', type=str, metavar='', help='')
-parser.add_argument('output_ori', type=str, metavar='', help='')
-parser.add_argument('output_mod', type=str, metavar='', help='')
+parser.add_argument('output_dir', type=str, metavar='', help='')
+parser.add_argument('pos_ori', type=int, metavar='', help='')
+parser.add_argument('pos_mod', type=int, metavar='', help='')
+
+# python BuildCFMatrix.py /media/Data/PTB/PTB_330/dev /home/lingpenk/parsing_friendly_tagger/data/ 1 2
 
 A = parser.parse_args()
 
@@ -53,6 +56,11 @@ if __name__ == '__main__':
     sys.stdout = codecs.getwriter('utf-8')(sys.stdout)
     sys.stderr = codecs.getwriter('utf-8')(sys.stderr)
 
+    # if the original pos the same as the modified one, there is no point to generate the data anyway
+    # so we just skip it
+    if A.pos_ori == A.pos_mod:
+        exit()
+
     corpus = read_corpus(A.inputf)
 
     POS_FIELD = 4
@@ -69,7 +77,52 @@ if __name__ == '__main__':
     # Let's say, we want to replace the pos tag pos_o with the pos tag pos_n
     # randomly and see how many more errors we get
 
-
+    POS_DICT = {
+        1 : "NN",
+        2 : "NNP",
+        3 : "IN",
+        4 : "DT",
+        5 : "NNS",
+        6 : "JJ",
+        7 : ",",
+        8 : "CD",
+        9 : "VBD",
+        10 : ".",
+        11 : "RB",
+        12 : "CC",
+        13 : "VB",
+        14 : "TO",
+        15 : "VBN",
+        16 : "VBZ",
+        17 : "PRP",
+        18 : "VBG",
+        19 : "POS",
+        20 : "$",
+        21 : "MD",
+        22 : "VBP",
+        23 : "``",
+        24 : "PRP$",
+        25 : "'",
+        26 : ":",
+        27 : "WDT",
+        28 : "JJR",
+        29 : "RP",
+        30 : "RBR",
+        31 : "WRB",
+        32 : "JJS",
+        33 : "WP",
+        34 : "-RRB-",
+        35 : "-LRB-",
+        36 : "EX",
+        37 : "RBS",
+        38 : "WP$",
+        39 : "#",
+        40 : "LS",
+        41 : "UH",
+        42 : "PDT",
+        43 : "FW",
+        44 : "NNPS"
+    }
 
     # If it is "NN" replaced by the "NNS", we probably won't worry so much about it...
     # pos_o = "NN"
@@ -77,8 +130,12 @@ if __name__ == '__main__':
 
     # If it is "NN" replaced by VBD, we probably will be very unhappy...
 
-    pos_o = "NN"
-    pos_n = "VBD"
+    # pos_o = "NN"
+    # pos_n = "VBD"
+
+    # read those pos tags from the command line
+    pos_o = POS_DICT[A.pos_ori]
+    pos_n = POS_DICT[A.pos_mod]
 
     original_corpus = []
     modified_corpus = []
@@ -99,12 +156,12 @@ if __name__ == '__main__':
             modified_corpus.append(sent)
 
 
-    output_ori = codecs.open(A.output_ori, "w", "utf-8")
+    output_ori = codecs.open(A.output_dir + "/ori_" + str(A.pos_ori) + "_" + str(A.pos_mod), "w", "utf-8")
     for sent in original_corpus:
         print_sentence(sent, output_ori)
     output_ori.close()
 
-    output_mod = codecs.open(A.output_mod, "w", "utf-8")
+    output_mod = codecs.open(A.output_dir + "/mod_" + str(A.pos_ori) + "_" + str(A.pos_mod), "w", "utf-8")
     for sent in modified_corpus:
         print_sentence(sent, output_mod)
     output_mod.close()
--- exp.sh
+++ exp.sh
@@ -2,20 +2,48 @@
 
 # running the parser
 
-./TurboParser --test --evaluate --file_model=/media/Data/models/210basic_sd330 --file_test=/home/lingpenk/parsing_friendly_tagger/data/dev_ori_nn_nns --file_prediction=/home/lingpenk/parsing_friendly_tagger/data/result/dev_ori_nn_nns.predicted --logtostderr
+# ./TurboParser --test --evaluate --file_model=/media/Data/models/210basic_sd330 --file_test=/home/lingpenk/parsing_friendly_tagger/data/dev_ori_nn_nns --file_prediction=/home/lingpenk/parsing_friendly_tagger/data/result/dev_ori_nn_nns.predicted --logtostderr
 
-./TurboParser --test --evaluate --file_model=/media/Data/models/210basic_sd330 --file_test=/home/lingpenk/parsing_friendly_tagger/data/dev_ori_nn_vbd --file_prediction=/home/lingpenk/parsing_friendly_tagger/data/result/dev_ori_nn_vbd.predicted --logtostderr
+# ./TurboParser --test --evaluate --file_model=/media/Data/models/210basic_sd330 --file_test=/home/lingpenk/parsing_friendly_tagger/data/dev_ori_nn_vbd --file_prediction=/home/lingpenk/parsing_friendly_tagger/data/result/dev_ori_nn_vbd.predicted --logtostderr
 
-./TurboParser --test --evaluate --file_model=/media/Data/models/210basic_sd330 --file_test=/home/lingpenk/parsing_friendly_tagger/data/dev_mod_nn_nns --file_prediction=/home/lingpenk/parsing_friendly_tagger/data/result/dev_mod_nn_nns.predicted --logtostderr
+# ./TurboParser --test --evaluate --file_model=/media/Data/models/210basic_sd330 --file_test=/home/lingpenk/parsing_friendly_tagger/data/dev_mod_nn_nns --file_prediction=/home/lingpenk/parsing_friendly_tagger/data/result/dev_mod_nn_nns.predicted --logtostderr
 
-./TurboParser --test --evaluate --file_model=/media/Data/models/210basic_sd330 --file_test=/home/lingpenk/parsing_friendly_tagger/data/dev_mod_nn_vbd --file_prediction=/home/lingpenk/parsing_friendly_tagger/data/result/dev_mod_nn_vbd.predicted --logtostderr
+# ./TurboParser --test --evaluate --file_model=/media/Data/models/210basic_sd330 --file_test=/home/lingpenk/parsing_friendly_tagger/data/dev_mod_nn_vbd --file_prediction=/home/lingpenk/parsing_friendly_tagger/data/result/dev_mod_nn_vbd.predicted --logtostderr
 
 # evaluation commands
 
-perl scripts/eval.pl -g /home/lingpenk/parsing_friendly_tagger/data/dev_ori_nn_nns -s /home/lingpenk/parsing_friendly_tagger/data/result/dev_ori_nn_nns.predicted >  /home/lingpenk/parsing_friendly_tagger/data/report/dev_ori_nn_nns.report
+# perl scripts/eval.pl -g /home/lingpenk/parsing_friendly_tagger/data/dev_ori_nn_nns -s /home/lingpenk/parsing_friendly_tagger/data/result/dev_ori_nn_nns.predicted >  /home/lingpenk/parsing_friendly_tagger/data/report/dev_ori_nn_nns.report
 
-perl scripts/eval.pl -g /home/lingpenk/parsing_friendly_tagger/data/dev_ori_nn_nns -s /home/lingpenk/parsing_friendly_tagger/data/result/dev_mod_nn_nns.predicted >  /home/lingpenk/parsing_friendly_tagger/data/report/dev_mod_nn_nns.report
+# perl scripts/eval.pl -g /home/lingpenk/parsing_friendly_tagger/data/dev_ori_nn_nns -s /home/lingpenk/parsing_friendly_tagger/data/result/dev_mod_nn_nns.predicted >  /home/lingpenk/parsing_friendly_tagger/data/report/dev_mod_nn_nns.report
 
-perl scripts/eval.pl -g /home/lingpenk/parsing_friendly_tagger/data/dev_ori_nn_vbd -s /home/lingpenk/parsing_friendly_tagger/data/result/dev_ori_nn_vbd.predicted >  /home/lingpenk/parsing_friendly_tagger/data/report/dev_ori_nn_vbd.report
+# perl scripts/eval.pl -g /home/lingpenk/parsing_friendly_tagger/data/dev_ori_nn_vbd -s /home/lingpenk/parsing_friendly_tagger/data/result/dev_ori_nn_vbd.predicted >  /home/lingpenk/parsing_friendly_tagger/data/report/dev_ori_nn_vbd.report
 
-perl scripts/eval.pl -g /home/lingpenk/parsing_friendly_tagger/data/dev_ori_nn_vbd -s /home/lingpenk/parsing_friendly_tagger/data/result/dev_mod_nn_vbd.predicted >  /home/lingpenk/parsing_friendly_tagger/data/report/dev_mod_nn_vbd.report
+# perl scripts/eval.pl -g /home/lingpenk/parsing_friendly_tagger/data/dev_ori_nn_vbd -s /home/lingpenk/parsing_friendly_tagger/data/result/dev_mod_nn_vbd.predicted >  /home/lingpenk/parsing_friendly_tagger/data/report/dev_mod_nn_vbd.report
+
+# Generate Data
+for i in {1..44}
+do
+	for j in {1..44}
+	do
+		if [ "$i" -ne "$j" ]; then
+				python BuildCFMatrix.py /media/Data/PTB/PTB_330/dev /media/Data/parsing_friendly_tagger/data/source/ ${i} ${j} 
+		fi
+	done
+done
+
+cd /home/lingpenk/TBP/TurboParser/
+export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:`pwd;`/deps/local/lib:"
+
+for i in {1..44}
+do
+	for j in {1..44}
+	do
+		if [ "$i" -ne "$j" ]; then
+				./TurboParser --test --evaluate --file_model=/media/Data/models/210basic_sd330 --file_test=/media/Data/parsing_friendly_tagger/data/source/ori_${i}_${j} --file_prediction=/media/Data/parsing_friendly_tagger/data/result/ori_${i}_${j}.predicted --logtostderr
+				./TurboParser --test --evaluate --file_model=/media/Data/models/210basic_sd330 --file_test=/media/Data/parsing_friendly_tagger/data/source/mod_${i}_${j} --file_prediction=/media/Data/parsing_friendly_tagger/data/result/mod_${i}_${j}.predicted --logtostderr
+				perl scripts/eval.pl -g /media/Data/parsing_friendly_tagger/data/source/ori_${i}_${j} -s /media/Data/parsing_friendly_tagger/data/result/ori_${i}_${j}.predicted >  /media/Data/parsing_friendly_tagger/data/report/ori_${i}_${j}.report
+				perl scripts/eval.pl -g /media/Data/parsing_friendly_tagger/data/source/ori_${i}_${j} -s /media/Data/parsing_friendly_tagger/data/result/mod_${i}_${j}.predicted >  /media/Data/parsing_friendly_tagger/data/report/mod_${i}_${j}.report
+		fi
+	done
+done
+
